0a986c4ef307b27b7697fab32bdf4ac1b94f4d88
lealobanov <lea.lobanov@icloud.com>
2023-10-10 21:37:37 +0900

    Onboarding content

---
+++ index.js
@@ -0,0 +1,26 @@
+// Pass the repo name
+const recipe = "vault-minter";
+
+//Generate paths of each code file to render
+const contractPath = `${recipe}/cadence/contract.cdc`;
+const transactionPath = `${recipe}/cadence/transaction.cdc`;
+
+//Generate paths of each explanation file to render
+const smartContractExplanationPath = `${recipe}/explanations/contract.txt`;
+const transactionExplanationPath = `${recipe}/explanations/transaction.txt`;
+
+export const vaultMinter = {
+  slug: recipe,
+  title: "Vault Minter",
+  description: "",
+  createdAt: Date(2022, 3, 1),
+  author: "Flow Blockchain",
+  playgroundLink:
+    "https://play.onflow.org/ef2fe054-148b-4c75-94f1-95bd33b6ce00?type=tx&id=899a81c3-a141-4021-a2b6-0e78ee8a105a",
+  excerpt:
+    "This resource is used by the admin to mint more tokens.",
+  smartContractCode: contractPath,
+  smartContractExplanation: smartContractExplanationPath,
+  transactionCode: transactionPath,
+  transactionExplanation: transactionExplanationPath,
+};
